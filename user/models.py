f25eac563efcd3ed6d4d49a6b70282e5de8f97d8
aya maher <aya.maher.m7md@gmail.com>
2024-04-03 03:47:30 +0200

    chore:update user model

--- user/models.py
+++ user/models.py
@@ -1,8 +1,42 @@
-from enum import Enum
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 from django.core.validators import RegexValidator
 # Create your models here.
-class User(models.Model):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email,password,first_name,last_name):
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not first_name:
+            raise ValueError('The First name field must be set')
+        if not last_name:
+            raise ValueError('The First name field must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email,first_name=first_name,last_name=last_name)
+        # user.gender = gender
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password,**extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        super_user = self.create_user(email=email, password=password, first_name=first_name, last_name=last_name)
+        super_user.is_staff = True
+        super_user.is_superuser = True
+        # super_user.gender = "M"
+        super_user.save()
+        return super_user
+
+
+
+class User(AbstractBaseUser):
 
     GENDER_CHOICES = [
         ("F", 'Female'),
@@ -26,7 +60,7 @@ class User(models.Model):
         ])
     email = models.EmailField(max_length=255,unique=True)
     password=models.CharField(
-        max_length=16,
+        max_length=255,
         validators=[
             RegexValidator(
                 
@@ -34,4 +68,21 @@ class User(models.Model):
                 message='', 
                 code='nomatch')
         ])
-    gender=models.CharField(max_length=6,choices=GENDER_CHOICES)
+    # gender=models.CharField(max_length=6,choices=GENDER_CHOICES,default="M",blank=True,null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser =  models.BooleanField(default=False)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ["first_name","last_name"]
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+    
+    def __str__(self):
+        return self.email
+    
