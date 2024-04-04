ceb77f87296ee66911f7b5cd33444db8f22eab5f
ayamaher205 <aya.maher.m7md@gmail.com>
2024-04-04 14:43:38 +0200

    refactore:refactore views code

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import (MyTokenObtainPairView,RegisterView)
+from .views import (LoginView,RegisterView)
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     #Authentication
-    path('signin', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('signin', LoginView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup', RegisterView.as_view(), name='auth_register'),
 ]
