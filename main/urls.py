98678c6a3cc6afdbd2c272e0cc51ffc89feb23f2
“sara-aref” <sara.aref.hassan@gmail.com>
2024-04-04 05:28:31 +0200

    add migrations

--- main/urls.py
+++ main/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
-    path('categories/', include('categories.urls'))
+    path('categories/', include('categories.urls')),
 ]
