cb35dfd4951a7e2a3387c14448690605d49081ba
v-m-q <abdomohamedbna@gmail.com>
2024-04-04 14:12:44 +0200

    Feat: update user data with jwt

--- user/urls.py
+++ user/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('user-profile-data', StudentAPI.as_view()),
+    path('', StudentAPI.as_view()),
+    path('update', StudentAPI.as_view()),
     
     
     # path('login', views.Login.as_view(), name='login'),
--- user/views.py
+++ user/views.py
@@ -57,60 +57,28 @@ class RegisterUser(APIView):
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.shortcuts import get_object_or_404
 
 class StudentAPI(APIView):
     authentication_classes = [ JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        # student_objs = Student.objects.all()
         print(request)
         userData = User.objects.filter(email=request.user)
         serializer = UserSerializer(userData, many=True)
         return Response({'status' : 200 , 'payload' : serializer.data})
     
-        # serializer = UserSerializer(User.objects.filter , many=True)
-        # return Response(serializer.data, status=status.HTTP_200_OK)
-       
-    def post(self, request):
-        serializer = UserSerializer(data = request.data)
-
-        if not serializer.is_valid():
-            print(serializer.errors)
-            return Response({'status' : 403 ,'errors' : serializer.errors , 'messge' : 'Something went wrong'})
-            
-        serializer.save()   
-        return Response({'status' : 200 , 'payload' : serializer.data , 'messge' : 'your data is saved'})
-
-   
     def put(self, request):
-        pass
-    
-    def patch(self,request):
-        try:
-            student_obj = User.objects.get(id = request.data['id'])
-            serializer = UserSerializer(student_obj , data = request.data , partial =True)
-            if not serializer.is_valid():
-                print(serializer.errors)
-                return Response({'status' : 403 ,'errors' : serializer.errors , 'messge' : 'Something went wrong'})
-                
-            serializer.save()   
-            return Response({'status' : 200 , 'payload' : serializer.data , 'messge' : 'your data is updated'})
-
-        except Exception as e:
-            print(e)
-            return Response({'status' :403 , 'message' : 'invalid id'})
+        print(request.user)
+        user_instance = get_object_or_404(User, email=request.user) #User.objects.filter(email=request.user).first()            
+        serializer = UserSerializer(user_instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 200, 'message': 'User updated successfully', 'payload': serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
    
-    def delete(self, request):
-        try:
-            id = request.GET.get('id')
-            student_obj = User.objects.get(id = id)    
-            student_obj.delete()
-            return Response({'status' : 200, 'message' : 'deleted'})
-    
-        except Exception as e:
-            print(e)
-            return Response({'status' :403 , 'message' : 'invalid id'})
+       
         
             
 
