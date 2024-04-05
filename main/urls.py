b7b8cfa5a3640f55c9edf65cd405c7f61d4bce14
v-m-q <abdomohamedbna@gmail.com>
2024-04-05 09:34:39 +0200

    fix: root main endpoint

--- main/urls.py
+++ main/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.getHomeData),
     path('pay/', include('payment.urls')),
     path('admin/', admin.site.urls),
     path('account/', include('user.urls')),
