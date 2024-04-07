a1f90443ef818fb18f898fe66bc0305d3753e619
v-m-q <abdomohamedbna@gmail.com>
2024-04-07 05:30:48 +0200

    v4

--- main/wsgi.py
+++ main/wsgi.py
@@ -15,4 +15,21 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application()
 
+class Inspect:
+    @staticmethod
+    def monkeypatch():
+        import inspect
+        print('Applying monkeypatch for inspect')
+        if hasattr(inspect, 'getfullargspec') and not hasattr(inspect, 'getargspec'):
+            def getargspec(func):
+                from collections import namedtuple
+                inspect_result = inspect.getfullargspec(func)
+                ArgSpec = namedtuple('ArgSpec', 'args varargs keywords defaults')
+                return ArgSpec(
+                    inspect_result.args, inspect_result.varargs,
+                    inspect_result.varkw, inspect_result.defaults
+                )
+            inspect.getargspec = getargspec
+
+Inspect.monkeypatch()
 # app = application
