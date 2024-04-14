afb9801c504042a1875475d54724d07711a2572a
ayamaher205 <aya.maher.m7md@gmail.com>
2024-04-14 04:22:08 +0200

    resolve static files

--- main/settings.py
+++ main/settings.py
@@ -197,7 +197,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://cartify-2.onrender.com/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
