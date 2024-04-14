32b576fa424a157d79ca77c313d4a5208f890539
ayamaher205 <aya.maher.m7md@gmail.com>
2024-04-14 04:59:31 +0200

    resolve static files

--- main/wsgi.py
+++ main/wsgi.py
@@ -16,3 +16,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 application = get_wsgi_application()
 
 app = application
+
+from main.wsgi import HelloWorldApplication
+
+application = HelloWorldApplication(application)
