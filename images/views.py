7c3479003498744d678e256e062c76b0864b8b66
v-m-q <abdomohamedbna@gmail.com>
2024-04-14 14:13:24 +0200

    fix: product images endpoint

--- images/views.py
+++ images/views.py
@@ -4,11 +4,13 @@ from rest_framework.decorators import api_view
 from .models import Images
 from .serializer import ImagesSerializer
 
+
 @api_view(['GET'])
 def productImages(request, ProductId):
     try:
         product_images = Images.objects.filter(product_id=ProductId)
-        serializer = ImagesSerializer(product_images)
+        serializer = ImagesSerializer(product_images,  many=True)
+        print(product_images)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    except product_images.DoesNotExist:
+    except Exception as e:
         return Response(status=status.HTTP_404_NOT_FOUND)
