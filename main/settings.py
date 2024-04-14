347892a004dc501d7055d487055faa63d18fef2b
ayamaher205 <aya.maher.m7md@gmail.com>
2024-04-14 04:18:07 +0200

    resolve static files

--- main/settings.py
+++ main/settings.py
@@ -198,10 +198,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static'
 ]
 
 MEDIA_URL = 'media/'
